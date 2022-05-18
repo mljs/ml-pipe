d4389b0014fd0baac1bf71f55ee4d725aeb8b3f2
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-05-18 08:39:02 +0200

    feat: implement SVR with options, closes #4

--- src/estimators/kernel/svm/SVR.ts
+++ src/estimators/kernel/svm/SVR.ts
@@ -3,16 +3,96 @@ import { Matrix } from 'ml-matrix';
 
 import { Estimator } from '../../Estimator';
 
+export interface SVROptions {
+  kernel?: 'rbf' | 'linear' | 'polynomial' | 'sigmoid';
+  // Type of SVR to perform. Epsilon and nu a different formulations of the penalty, see https://wiki.eigenvector.com/index.php?title=Svm
+  type?: 'nu' | 'epsilon';
+  // Degree of the polynomial kernel function (only for polynomial kernel).
+  degree?: number;
+  // Gamma parameter (only for rbf and poly kernels).
+  gamma?: number;
+  // Coefficient in kernel function (only for rbf and poly kernels).
+  coef0?: number;
+  // Nu parameter in loss function of Nu SVR (only for nu SVR).
+  nu?: number;
+  // Epsilon parameter in loss function of Epsilon SVR (only for epsilon SVR).
+  epsilon?: number;
+  // Value(s) to use for LIBSVM 'c' parameter.
+  cost?: number;
+  // cache size in MB
+  cacheSize?: number;
+  // tolerance of termination criterion
+  tolerance?: number;
+  // whether to use the shrinking heuristics
+  shrinking?: boolean;
+  // whether to train a SVR model for probability estimates
+  probabilityEstimates?: boolean;
+  // print info during learning
+  quiet?: boolean;
+}
+
+// ToDo: check that this works as expected
+const reg = new FinalizationRegistry((model: any) => {
+  if ('free' in model) {
+    model.free();
+  }
+});
+
 export class SVR implements Estimator {
   public model: any;
   public fitted: boolean;
-  public constructor() {
+  private options: SVROptions;
+  public constructor(options: SVROptions = {}) {
     this.model = undefined;
     this.fitted = false;
+    this.options = options;
+    reg.register(this, this.model);
+  }
+
+  private setOptions(options: SVROptions, numFeatures: number) {
+    const defaultOptions = {
+      kernel: 'rbf',
+      type: 'epsilon',
+      degree: 3,
+      gamma: 'auto',
+      coef0: 0,
+      nu: 0.5,
+      epsilon: 0.1,
+      cost: 1,
+      cacheSize: 200,
+      tolerance: 0.001,
+      shrinking: true,
+      probabilityEstimates: false,
+      quiet: true,
+    };
+
+    const opt = { ...defaultOptions, ...options };
+
+    if (opt.gamma === 'auto') {
+      opt.gamma = 1 / (numFeatures + 1);
+    }
+
+    if (opt.type === 'epsilon') {
+      opt.type = SVM.SVM_TYPES.EPSILON_SVR;
+    } else if (opt.type === 'nu') {
+      opt.type = SVM.SVM_TYPES.NU_SVR;
+    }
+    if (opt.kernel === 'rbf') {
+      opt.kernel = SVM.KERNEL_TYPES.RBF;
+    } else if (opt.kernel === 'polynomial') {
+      opt.kernel = SVM.KERNEL_TYPES.POLYNOMIAL;
+    } else if (opt.kernel === 'sigmoid') {
+      opt.kernel = SVM.KERNEL_TYPES.SIGMOID;
+    } else if (opt.kernel === 'linear') {
+      opt.kernel = SVM.KERNEL_TYPES.SIGMOID;
+    }
+
+    return opt;
   }
 
   public async fit(X: Matrix, y: Matrix) {
-    this.model = new SVM({ type: SVM.SVM_TYPES.EPSILON_SVR });
+    this.setOptions(this.options, X.columns);
+    this.model = new SVM(this.options);
     const prom = new Promise((resolve) => {
       resolve(this.model.train(X.to2DArray(), y.to1DArray()));
     });
--- src/metrics/__tests__/complexity.test.ts
+++ src/metrics/__tests__/complexity.test.ts
@@ -6,8 +6,8 @@ import { trainingSet, labels } from '../../utils/testHelpers';
 import { generalizedDegreesOfFreedom } from '../complexity';
 
 describe('test generalized degrees of freedom', () => {
-  seedrandom('test');
   it('on linear regressor', async () => {
+    seedrandom('test');
     const regressor = new LinearRegressor();
     await regressor.fit(trainingSet, labels);
     const gdf = await generalizedDegreesOfFreedom(
@@ -19,6 +19,7 @@ describe('test generalized degrees of freedom', () => {
   });
 
   it('on random forest regressor', async () => {
+    seedrandom('test');
     const regressor = new RandomForestRegressor();
     await regressor.fit(trainingSet, labels);
     const gdf = await generalizedDegreesOfFreedom(
